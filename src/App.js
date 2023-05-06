9f5c05745aa3b276a9ccc7c3b33ea520067b0301
DESKTOP-0MGKQIM\TCO-Training <tco@tco.am>
2023-05-06 20:32:57 +0400

    console.log

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  console.log('55');
   return <h2>reminder project setup</h2>;
 }
 
